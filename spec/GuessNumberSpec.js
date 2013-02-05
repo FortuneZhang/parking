793c6934a8f3fa460b089292390d89618ae79f6c
FortuneZhang <zhangfortune527@gmail.com>
2013-02-05 13:01:44 +0800

    bind guess_number and create number module

---
+++ spec/GuessNumberSpec.js
@@ -0,0 +1,95 @@
+describe("Guess Number ", function() {
+
+    var origin;
+
+    beforeEach(function() {
+        origin =  [1,2,3,4];
+    });
+
+    it("given origin number [1,2,3,4], when input [5,6,7,8] ,then result is '0A0B' ", function() {
+        var numbers = [5,6,7,8];
+        expect(guess_num(origin,numbers)).toBe("0A0B");
+    });
+
+    it("given origin number [1,2,3,4], when input [1,2,3,4] ,then result is '4A0B' ", function() {
+        var numbers = [1,2,3,4];
+        expect(guess_num(origin,numbers)).toBe("4A0B");
+    });
+
+    it("given origin number [1,2,3,4], when input [4,3,2,1] ,then result is '0A4B' ", function() {
+        var numbers = [4,3,2,1];
+        expect(guess_num(origin,numbers)).toBe("0A4B");
+    });
+
+    it("given origin number [1,2,3,4], when input [1,2,4,3] ,then result is '2A2B' ", function() {
+        var numbers = [1,2,4,3];
+        expect(guess_num(origin,numbers)).toBe("2A2B");
+    });
+
+});
+describe("create 4 different numbers ",function(){
+
+        var created_number;
+        beforeEach(function(){
+            created_number = create_four_numbers()
+        }) ;
+        it("the created numbers are number",function(){
+            expect(is_number(created_number)).toBe(true);
+        });
+        it("the created numbers is 4 bit",function(){
+           expect(is_four_bit(created_number)).toBe(true)
+        });
+        it("each bit of the created numbers is different from others",function(){
+            expect(is_different(created_number)).toBe(true);
+        });
+        it("given target_number is '1234' ,when created_number is not equal '1234' the created number is random ",function(){
+            expect(is_random_number()).toBe(true);
+        });
+})
+
+describe("test all test helpers  ",function(){
+    it("test is_random_number() function",function(){
+        expect(is_random_number()).toBe(true);
+    });
+
+    describe("test is_number() function ",function(){
+        it("when input '1234' then result is true",function(){
+            expect(is_number("1234")).toBe(true);
+        });
+        it("given '121b'  then result is false",function(){
+            expect(is_number("121b")).toBe(false);
+        });
+        it("given '~21b'  then result is false",function(){
+            expect(is_number("~21b")).toBe(false);
+        });
+    });
+    describe("test is_four_bit() function",function(){
+        it("when input '1234' then result should be true ",function(){
+            expect(is_four_bit("1234")).toBe(true);
+        })
+        it("when input '123'  then result should be false",function(){
+            expect(is_four_bit("123")).toBe(false);
+        })
+        it("when input '12345' then result should be false ",function(){
+            expect(is_four_bit("12345")).toBe(false);
+        })
+        it("when input '!2f.'then result should be false ",function(){
+            expect(is_four_bit("!2f.")).toBe(true);
+        })
+    });
+});
+
+describe("bind guess number and create number",function (){
+        it('given random number 1234, when user guess with 1234, the result should be 4A0B',function(){
+            var create_random_number_func = function(){
+                return "1234";
+            }
+            var game = new Game(create_random_number_func);
+            var result = game.play("1234")
+            expect(result).toBe('4A0B')
+        })
+
+
+
+
+})
---
+++ src/source.js
@@ -0,0 +1,165 @@
+function guess_num(origin,numbers)
+{
+    var A_count = 0;
+    var B_count = 0;
+    var i = 0, j =0;
+    for(i=0;i < 4; i++)
+    {
+        if(origin[i] == numbers[i])
+            A_count++;
+    }
+    for(i=0; i < 4; i ++)
+    {
+        for(j =0 ; j < 4; j ++)
+        {
+            if(origin[i] == numbers[j])
+                B_count ++;
+        }
+    }
+    return A_count+"A" + (B_count-A_count) +"B";
+}
+
+function create_four_numbers()
+{
+    var numbers ="";
+    var random_number =Math.floor(Math.random()*9);
+    numbers +=random_number;
+    var i =0;
+
+    while(numbers.length !=4 )
+    {
+        random_number =Math.floor(Math.random()*9);
+        for(i =0; i < numbers.length; i +=1)
+        {
+            if(numbers[i] ==random_number)
+            {
+                break;
+            }
+        }
+        if(i >=  numbers.length)
+        {
+              numbers +=random_number;
+        }
+    }
+    return numbers;
+}
+function is_four_bit(number)
+{
+    if(number.length !=4)
+        return false;
+    return true;
+
+//    return number >123 && number < 9876 ?true:false;
+}
+function is_different(number)
+{
+    var i=0;
+    var j=0;
+    var result =true;
+    for(i=0;i <4; i +=1)
+    {
+        if(!result )
+        {
+            break;
+        }
+        for(j=i;j < 3; j ++ )
+        {
+           if(number[j] == number[j+=1])
+           {
+               result =false;
+               break;
+           }
+        }
+    }
+    return result;
+}
+function is_random_number()
+{
+    var result = true;
+    var test_counts =10;
+    var created_numbers =[];
+    for(var i =test_counts;i >0 ;i -=1)
+    {
+        created_numbers.push(create_four_numbers());
+    }
+    for(var i =0; i< test_counts;i +=1)
+    {
+        if(!result)
+        {
+            return;
+        }
+        for(var j =i;j <test_counts-1; j+=1)
+        {
+                      if(created_numbers[j] == created_numbers[j+1])
+                      {
+                          result =false;
+                          break;
+              }
+        }
+    }
+    return result;
+}function is_random_number()
+{
+    var result = true;
+    var test_counts =10;
+    var created_numbers =[];
+    for(var i =test_counts;i >0 ;i -=1)
+    {
+        created_numbers.push(create_four_numbers());
+    }
+    for(var i =0; i< test_counts;i +=1)
+    {
+        if(!result)
+        {
+            return;
+        }
+        for(var j =i;j <test_counts-1; j+=1)
+        {
+                      if(created_numbers[j] == created_numbers[j+1])
+                      {
+                          result =false;
+                          break;
+              }
+        }
+    }
+    return result;
+}
+function is_random_number() {
+    var result = true;
+    var test_counts = 10;
+    var created_numbers = [];
+    for (var i = test_counts; i > 0; i -= 1) {
+        created_numbers.push(create_four_numbers());
+    }
+    for (var i = 0; i < test_counts; i += 1) {
+        if (!result) {
+            return;
+        }
+        for (var j = i; j < test_counts - 1; j += 1) {
+            if (created_numbers[j] == created_numbers[j + 1]) {
+                result = false;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+function is_number(arg) {
+    return !isNaN(arg)
+}
+
+
+function play_game()
+{
+    var random_number =create_four_numbers();
+}
+
+function Game(create_random_number_func){
+    this.create_random_number_func = create_random_number_func;
+}
+
+Game.prototype.play = function(number){
+    var origin =this.create_random_number_func()
+    return guess_num(origin,number);
+}
