72c54eac56e860456a336092b5ae0ab3a3e41a04
FortuneZhang <zhangfortune527@gmail.com>
2013-02-18 14:06:26 +0800

    delete available property

--- spec/parking_lot_spec.js
+++ spec/parking_lot_spec.js
@@ -1,26 +1,34 @@
 describe("parking lot",function(){
-    it("capacity is 10,available is 0 ,when park a car ,it should fail",function(){
-        var parking_lot = new ParkingLot(10,0);
+
+    it("capacity is 0,when park a car ,it should fail",function(){
+        var parking_lot = new ParkingLot(0);
 
         var result =parking_lot.park_a_car();
 
-        expect(result).toBe(false);
+        expect(result).toBe("fail");
     });
 
     it("capacity is 10,available is 10,when park a car,it should succeed",function(){
-        var parking_lot = new ParkingLot(10,10);
+        var parking_lot = new ParkingLot(10);
 
         var result = parking_lot.park_a_car();
 
-        expect(result).toBe(true);
+        expect(result).toBe("success");
     });
 
-    it("capacity is 10,available is 1,when park a car,available should be 0",function(){
-        var parking_lot = new ParkingLot(10,1);
+    it("capacity is 10,when park a car,capacity should be 9",function(){
+        var parking_lot = new ParkingLot(10);
 
         parking_lot.park_a_car();
 
-        expect(parking_lot.available).toBe(0);
+        expect(parking_lot.capacity).toBe(9);
     }) ;
 
+    it("available is 10,when get available count ,it should be 10",function(){
+        var parking_lot = new ParkingLot(10);
+
+        var capacity_count = parking_lot.get_capacity_count();
+
+        expect(capacity_count).toBe(10);
+    });
 });
--- src/parking_lot_source.js
+++ src/parking_lot_source.js
@@ -1,14 +1,17 @@
-function ParkingLot(capacity,available)
+function ParkingLot(capacity)
 {
     this.capacity = capacity;
-    this.available = available;
 }
 ParkingLot.prototype.park_a_car = function()
 {
-    if(this.available == 0)
+    if(this.capacity == 0)
     {
-        return false;
+        return "fail";
     }
-    this.available -=1;
-    return true;
+    this.capacity -=1;
+    return "success";
+}
+ParkingLot.prototype.get_capacity_count = function()
+{
+    return this.capacity;
 }
