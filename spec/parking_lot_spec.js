93820fb5e1679792b9c3eb1e0818a2367dc1ad30
FortuneZhang <zhangfortune527@gmail.com>
2013-02-21 13:36:19 +0800

    un pick a car

--- spec/parking_lot_spec.js
+++ spec/parking_lot_spec.js
@@ -3,32 +3,123 @@ describe("parking lot",function(){
     it("capacity is 0,when park a car ,it should fail",function(){
         var parking_lot = new ParkingLot(0);
 
-        var result =parking_lot.park_a_car();
+        var result =parking_lot.parking_a_car(1);
 
         expect(result).toBe("fail");
     });
 
-    it("capacity is 10,available is 10,when park a car,it should succeed",function(){
+    it("capacity is 10,available is 10,when park a car,position  should  0",function(){
         var parking_lot = new ParkingLot(10);
 
-        var result = parking_lot.park_a_car();
+        var result = parking_lot.parking_a_car(1);
 
-        expect(result).toBe("success");
+        expect(result).toBe(0);
     });
 
     it("capacity is 10,when park a car,capacity should be 9",function(){
         var parking_lot = new ParkingLot(10);
 
-        parking_lot.park_a_car();
+        parking_lot.parking_a_car(1);
 
-        expect(parking_lot.capacity).toBe(9);
+        expect(parking_lot.get_available_count()).toBe(9);
     }) ;
 
     it("available is 10,when get available count ,it should be 10",function(){
         var parking_lot = new ParkingLot(10);
 
-        var capacity_count = parking_lot.get_capacity_count();
+        var capacity_count = parking_lot.get_available_count();
 
         expect(capacity_count).toBe(10);
     });
+
 });
+ describe("parking boy control a parking lot",function(){
+     var result ;
+     var parking_lot ;
+     var parking_boy ;
+     beforeEach(function(){
+         parking_lot = new ParkingLot(10);
+         parking_boy = new ParkingBoy(parking_lot);
+     })
+
+
+     it("parking lot is  full ,when parking boy park a car,it should be fail",function(){
+         parking_lot = new ParkingLot(0);
+         parking_boy = new ParkingBoy(parking_lot);
+         expect(parking_boy.parking_lot.get_available_count()).toBe(0);
+
+         var result =parking_boy.parking_a_car(1);
+
+         expect(result).toBe("fail");
+     });
+
+     it("parking lot is not full ,when parking boy park a car,it should be 0",function(){
+         expect(parking_boy.parking_lot.get_available_count()).not.toBe(0);
+
+         var result =parking_boy.parking_a_car(1);
+
+         expect(result).toBe(0);
+     });
+
+     it('parking lot is not full ,available is 10,when parking boy park a car,then available is 9', function () {
+         parking_boy.parking_a_car(1);
+
+         result = parking_boy.get_available_count();
+         expect(result).toBe(9);
+     });
+
+     it('parking lot available is 10 and has pork 3 car,when parking boy park a car,then position is 3', function () {
+         parking_boy.parking_a_car(1);
+         parking_boy.parking_a_car(2);
+         parking_boy.parking_a_car(3);
+
+         result = parking_boy.parking_a_car(4);
+
+         expect(result).toBe(3);
+     });
+
+     it("has park a car number is 1, when I un park a car ,I can get the 1",function(){
+         var ticket;
+         ticket = parking_boy.parking_a_car(1);
+
+         result = parking_boy.un_parking_a_car(ticket);
+
+         expect(result).toBe(1);
+     });
+
+     it("has park 3 cars,when parking boy un park a car, when i can get available will increase",function(){
+         var before_up_park_available_count;
+         var after_up_park_available_count;
+         var ticket;
+         parking_boy.parking_a_car(233);
+         parking_boy.parking_a_car(225);
+         ticket = parking_boy.parking_a_car(12);
+         parking_boy.parking_a_car(978);
+         before_up_park_available_count = parking_boy.get_available_count();
+
+         parking_boy.un_parking_a_car(ticket) ;
+         after_up_park_available_count = parking_boy.get_available_count();
+
+         expect(after_up_park_available_count).toBe(before_up_park_available_count + 1) ;
+     }) ;
+
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/parking_lot_source.js
+++ src/parking_lot_source.js
@@ -1,17 +1,61 @@
 function ParkingLot(capacity)
 {
-    this.capacity = capacity;
+    this.parking_position = new Array(capacity);
+    this.available_count = capacity;
 }
-ParkingLot.prototype.park_a_car = function()
-{
-    if(this.capacity == 0)
-    {
+ParkingLot.prototype.parking_a_car = function (car_number) {
+    var position = 0;
+    if (this.available_count == 0) {
         return "fail";
     }
-    this.capacity -=1;
-    return "success";
-}
-ParkingLot.prototype.get_capacity_count = function()
+    for (position; position < this.parking_position.length; position += 1) {
+        if (this.parking_position[position] == undefined || this.parking_position[position] == -1) {
+            break;
+        }
+    }
+    this.available_count -= 1;
+    this.parking_position[position] = car_number;
+    return position;
+};
+
+ParkingLot.prototype.get_available_count = function()
 {
-    return this.capacity;
+    return this.available_count;
+} ;
+
+ParkingLot.prototype.un_parking_a_car = function(ticket)
+{
+    var car_number = this.parking_position[ticket];
+    this.parking_position[ticket] = -1;
+    this.available_count += 1;
+    return car_number;
 }
+
+
+function ParkingBoy(parking_lot)
+{
+    this.parking_lot =parking_lot;
+}
+
+ParkingBoy.prototype.parking_a_car = function(car_number)
+{
+   return  this.parking_lot.parking_a_car(car_number);
+};
+
+ParkingBoy.prototype.get_available_count = function()
+{
+    return this.parking_lot.get_available_count();
+};
+
+ParkingBoy.prototype.un_parking_a_car = function(ticket)
+{
+    return this.parking_lot.un_parking_a_car(ticket);
+}
+
+
+
+
+
+
+
+
